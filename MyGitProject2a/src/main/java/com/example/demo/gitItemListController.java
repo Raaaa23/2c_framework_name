a92ae3f3a799709cc9f81431fc4a8bf8f39acf28
S0532 <S0532@19S0532>
2023-11-17 14:31:49 +0900

    aaaa

---
+++ MyGitProject2a/src/main/java/com/example/demo/gitItemListController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class gitItemListController {
+
+
+     //画面表示用
+     @RequestMapping(path = "/gititemlist",method = RequestMethod.GET )
+     public String doListGet() {
+    	 //htmlを表示
+    	 return"gititemlist";
+     }
+     }
